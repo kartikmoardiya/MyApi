32bf29ec602ba8b2db6cb4cda20abb6c98f77bea
Man Dhanani <mandhanani96@gmail.com>
2024-05-16 21:32:17 +0530

    first commit

--- employee_router.js
+++ employee_router.js
@@ -5,7 +5,16 @@ const router = express.Router()
 const employee = require('./employee');
 
 router.get("/",async(req,res)=>{
-    const x =await  employee.find();
+    const x =await employee.find();
     res.json({x});
 })
+router.post("/create", async (req, resp) => {
+    let data = new employee(req.body);
+    let result = await data.save();
+    resp.send(result);
+  });
+  router.delete("/delete/:_id", async (req, resp) => {
+    let data = await employee.deleteOne(req.params);
+    resp.send(data);
+  });
 module.exports = router;
