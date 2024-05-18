150d1378ecaaa454d859dfaab74ebd954e645fe9
Man Dhanani <mandhanani96@gmail.com>
2024-05-18 18:00:42 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const product_router = require('./employee_router')
 const body_pardser = require('body-parser')
 
 app.use(body_pardser.json());
-app.use('/employee',product_router)
+app.use('/employees',product_router)
 const PORT = process.env.PORT||3000;
 
 const passport = require('./auth');
