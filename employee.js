99cab7f8d1b303fd08d8ecfdb50aa23d43e8e152
Man Dhanani <mandhanani96@gmail.com>
2024-05-18 18:25:03 +0530

    first commit

--- employee.js
+++ employee.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const employeeSchema = new mongoose.Schema({
     name:String,
@@ -11,5 +12,27 @@ const employeeSchema = new mongoose.Schema({
         required:true,
         type:String
     }
+});
+employeeSchema.pre('save',async function(next){
+    const person = this;
+    if(!person.isModified('password')){
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(person.password,salt);
+        person.password = hashPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
 })
+employeeSchema.methods.comparePassword  = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+    } catch (err) {
+        return err;
+    }
+}
 module.exports = mongoose.model('employee',employeeSchema);
--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const product_router = require('./employee_router')
 const body_pardser = require('body-parser')
 
 app.use(body_pardser.json());
-app.use('/employees',product_router)
+app.use('/employee',product_router)
 const PORT = process.env.PORT||3000;
 
 const passport = require('./auth');
-
 
 app.use(passport.initialize());
 const localAuthmiddleware = passport.authenticate('local',{session:false});
 app.get('/',localAuthmiddleware,function(req,res){
     res.send('welcome to our hotel');
 })
-
 
 app.listen(PORT,()=>{
     console.log.apply("Listening on port")
