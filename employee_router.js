5795c619338d350cd814b78f95d0b40c9ca22b62
Man Dhanani <mandhanani96@gmail.com>
2024-05-18 17:58:36 +0530

    first commit

--- employee_router.js
+++ employee_router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const router = express.Router()
 
-
+const employee = require('./employee');
 
 router.get("/",async(req,res)=>{
     const x =await employee.find();
