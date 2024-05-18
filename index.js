298a790c7519ef532a667473ed477ca6d690fb48
Man Dhanani <mandhanani96@gmail.com>
2024-05-18 17:36:10 +0530

    first commit

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const product_router = require('./employee_router')
 const body_pardser = require('body-parser')
 
 app.use(body_pardser.json());
-app.use('/employee',product_router)
+
 const PORT = process.env.PORT||3000;
-
 const passport = require('./auth');
-
-
 
 app.use(passport.initialize());
 const localAuthmiddleware = passport.authenticate('local',{session:false});
-app.get('/',localAuthmiddleware,function(req,res){
+app.get('/',function(req,res){
     res.send('welcome to our hotel');
 })
-
-
+app.use('/employee',localAuthmiddleware,product_router)
 app.listen(PORT,()=>{
     console.log.apply("Listening on port")
 })
